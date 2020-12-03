c888cad4872d0d3e58aa1721dbaaa92b9def16fc
rlagudtn <34295144+rlagudtn@users.noreply.github.com>
2020-12-03 10:34:03 +0900

    re

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -8,16 +8,21 @@ import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.ListView;
 import android.widget.Spinner;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    ListView lv_people;
+    String [] items={"ㅅ","아너미ㅏㅓ라"};
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.person_list_layout);
 
+        lv_people=(ListView)findViewById(R.id.lv_people);
+        ArrayAdapter adapter=new ArrayAdapter(this, android.R.layout.simple_list_item_1,items);
+        lv_people.setAdapter(adapter);
 
     }
 }
---
+++ app/src/main/java/com/example/myapplication/Person.java
@@ -0,0 +1,25 @@
+package com.example.myapplication;
+
+import java.io.Serializable;
+
+public class Person implements Serializable {
+        String name;
+        String phoneNumber;
+        String group;
+        String birth;
+        String memo;
+
+        public Person(){}
+        public Person(String name,String phoneNumber){
+            this.name=name;
+            this.phoneNumber=phoneNumber;
+            this.group="기타";
+        }
+    public Person(String name,String phoneNumber,String group,String birth,String memo){
+        this.name=name;
+        this.phoneNumber=phoneNumber;
+        this.group=group;
+        this.birth=birth;
+        this.memo=memo;
+    }
+}
